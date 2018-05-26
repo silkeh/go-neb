e0f6ef3887d5b14b880142b37f8bf4cacba638c8
Silke Hofstra <silke@slxh.eu>
2018-05-26 13:59:36 +0200

    Update CI for vgo

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -6,7 +6,7 @@ docker run \
     --rm \
     -v "$PWD":/usr/src/myapp \
     -w /usr/src/myapp \
-    golang:1.8-alpine \
-    sh -c 'apk add --no-cache git gcc musl-dev && go get -u github.com/constabulary/gb/... && gb build -f github.com/matrix-org/go-neb'
+    golang:1.10-alpine \
+    sh -c 'apk add --no-cache git gcc musl-dev && go get -u golang.org/x/vgo && vgo build'
 
 docker build -t ${IMAGE_PREFIX}go-neb:latest .
